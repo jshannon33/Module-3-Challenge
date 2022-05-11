0abb4ed5c5268a0dfdaa3ebf65ccdad38e4f1b80
Jason Shannon <jshannon3343@gmail.com>
2022-05-11 16:14:11 -0500

    first generate button toggles

--- generator.js
+++ generator.js
@@ -1,3 +1,11 @@
-document.getElementById('generateButton').onclick = function(){
-    
+
+function TestFunction() {
+    var T = document.getElementById("TestSection");
+    displayValue = "";
+    if (T.style.display == "")
+        displayValue = "none";
+
+    T.style.display = displayValue;
 }
+
+document.getElementById('length1').value;

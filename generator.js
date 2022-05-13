e28ce1fd2c00d6477facbbd36c1502237aef75cb
Jason Shannon <jshannon3343@gmail.com>
2022-05-12 19:38:37 -0500

    event listener not working

--- generator.js
+++ generator.js
@@ -8,4 +8,50 @@ function TestFunction() {
     T.style.display = displayValue;
 }
 
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numberEl = document.getElementById('number');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+
+}
+
+generateEl.addEventListener('click', () => {
+const length = +lengthEl.value;
+const hasLower = lowercaseEl.checked;
+const hasUpper = uppercaseEl.checked;
+const hasNumber = numberEl.checked;
+const hasSymbol = symbolsEl.checked;
+console.log(hasLower, hasUpper, hasNumber, hasSymbol);
+});
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+console.log(getRandomLower());
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+console.log(getRandomUpper());
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log(getRandomNumber());
+
+function getRandomSymbol() {
+    const symbols = "!@#$%^&*+=<>,./";
+    return symbols[Math.floor(Math.random() * symbols.length)]; 
+}
+console.log(getRandomSymbol())
 

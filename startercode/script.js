8354607595de0fc906b65a941e3c5b6a0e01abae
Jason Shannon <jshannon3343@gmail.com>
2022-05-14 21:52:26 -0500

    i dont understand this

--- startercode/script.js
+++ startercode/script.js
@@ -1,23 +1,10 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
+let characters = prompt('enter password length');
+passwordLength = parseInt(characters);
 
-    
-    console.log('you pressed the button');
-}
+const numbers = [1,2,3,4,5,6,7,8,9,0];
+const lowercase = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+const uppercase = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+const symbols = ['!','@','#','$','%','^','&','*','(',')','<','>','?']
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
